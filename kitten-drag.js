38bfd0044b14a81145fc7d8766a56575fb7f58ce
Dillon Plummer <dillon.plummer@gmail.com>
2020-08-27 16:04:26 -0700

    test
    test

--- kitten-drag.js
+++ kitten-drag.js
@@ -1,2 +1,24 @@
 import interact from
 'https://cdn.interactjs.io/v1.9.22/interactjs/index.js'
+
+interact('.item')
+  .draggable({
+    inertia: false,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    autoScroll: true,
+
+    listeners: {
+      // called on every Drag Move event
+      move: dragMoveListener,
+
+      // called on every Drag End event
+      end(event) {
+        var
+      }
+    }
+  })
